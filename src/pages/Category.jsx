e385884357ee8afface1d15bfe4d9578c35428b9
shabeeb <shabeebkaip@gmail.com>
2023-03-27 15:29:47 +0530

    category alignment fix

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -37,10 +37,10 @@ const Category = () => {
         category && category.name ?
           <div className="mb-10" id="hotDrinks" >
             <h1 className="mb-4 text-xl font-semibold text-center uppercase text-customYellow">{category.name}</h1>
-            <div className="flex flex-wrap items-center gap-2 my-2" align="center">
+            <div className="flex flex-wrap items-center justify-center gap-2 my-6" align="center">
               {category.subCategories?.map((item, index) => (
                 <div
-                  className={`cursor-pointer text-sm  px-2 py-1 flex items-center uppercase border text-customYellow border-customYellow rounded-2xl ${tabIndex === index ? "bg-customYellow text-white" : ''} `}
+                  className={`cursor-pointer text-sm  px-2 py-1 flex items-center  uppercase border text-customYellow border-customYellow rounded-2xl ${tabIndex === index ? "bg-customYellow text-white" : ''} `}
                   align="center"
                   key={index}
                   onClick={() => handleTabChange(index)}>
